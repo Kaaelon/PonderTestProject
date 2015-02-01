a17e4a7a32dd2837c7c01f84ec88db7408a3a05d
Kaaelon <mfreemancontact@gmail.com>
2015-02-02 00:49:45 +1100

    Edited textviews and wired up clicklisteners

--- app/src/main/java/testproject/halfmoonstudios/com/ponder/MenuFragment.java
+++ app/src/main/java/testproject/halfmoonstudios/com/ponder/MenuFragment.java
@@ -1,21 +1,27 @@
 package testproject.halfmoonstudios.com.ponder;
 
 import android.app.Fragment;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
-import testproject.halfmoonstudios.com.ponder.R;
+import android.widget.TextView;
 
 public class MenuFragment extends Fragment {
-
+    //Declares imageviews
     private ImageView mWellbeingView;
     private ImageView mGriefView;
     private ImageView mHealthView;
     private ImageView mIdeasView;
     private ImageView mMotivationView;
+    //Declares textviews
+    private TextView mWellbeingText;
+    private TextView mGriefText;
+    private TextView mHealthText;
+    private TextView mIdeasText;
+    private TextView mMotivationText;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,30 +32,74 @@ public class MenuFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_menu,parent,false);
         //Could be a problem moving into seperate method
 
-        //Assigns values to buttons
+        //Assigns values to ImageViews
        mWellbeingView = (ImageView)v.findViewById(R.id.wellbeingView);
        mGriefView = (ImageView)v.findViewById(R.id.griefView);
        mHealthView = (ImageView)v.findViewById(R.id.healthView);
        mIdeasView = (ImageView)v.findViewById(R.id.ideasView);
        mMotivationView = (ImageView)v.findViewById(R.id.motivationView);
 
-        mWellbeingView.setOnClickListener(new View.OnClickListener(){
+        //Assigns values to TextViews
+        mWellbeingText = (TextView)v.findViewById(R.id.wellbeingText);
+        mGriefText = (TextView)v.findViewById(R.id.griefText);
+        mHealthText = (TextView)v.findViewById(R.id.healthText);
+        mIdeasText = (TextView)v.findViewById(R.id.ideasText);
+        mMotivationText = (TextView)v.findViewById(R.id.motivationText);
 
-                                        @Override
-                                    public void onClick(View v){
+        //Create TypeFaceObject and assign font
+        Typeface mtypeFace = Typeface.createFromAsset(getActivity().getResources().getAssets(),"futuralight.ttf");
 
+        //Sets text of TextViews
+        mWellbeingText.setTypeface(mtypeFace);
+        mGriefText.setTypeface(mtypeFace);
+        mHealthText.setTypeface(mtypeFace);
+        mIdeasText.setTypeface(mtypeFace);
+        mMotivationText.setTypeface(mtypeFace);
 
-
-
-                                        }
-
-                                    }
-        );
+        //Set onclick listeners for textviews
+        setListeners();
         return v;
     }
 
-    public void drawGUI(){
+    public void setListeners(){
+     mWellbeingView.setOnClickListener(new View.OnClickListener(){
 
+      @Override
+      public void onClick(View v){
+
+            }
+        });
+
+    mGriefView.setOnClickListener(new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v){
+
+        }
+    });
+
+     mHealthView.setOnClickListener(new View.OnClickListener(){
+
+       @Override
+       public void onClick(View v){
+
+            }
+        });
+
+     mIdeasView.setOnClickListener(new View.OnClickListener(){
+
+       @Override
+       public void onClick(View v){
+
+            }
+        });
+     mMotivationView.setOnClickListener(new View.OnClickListener(){
+
+     @Override
+     public void onClick(View v){
+
+            }
+        });
     }
 
 
