2823e0f1f712d5a7497f44e01c89725190e2df53
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-11 21:14:49 +1100

    Added Async Task class that will be used to get Quotes file on background, add method that checks if network is available on the user's phone

--- app/src/main/java/testproject/halfmoonstudios/com/ponder/MainActivity.java
+++ app/src/main/java/testproject/halfmoonstudios/com/ponder/MainActivity.java
@@ -5,10 +5,26 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 /**
  * MainActivity - default activity containing setup information for the app, plus management of fragments
@@ -22,6 +38,7 @@ public class MainActivity extends Activity {
     private CountDownTimer cd;
     private FragmentManager fm = getFragmentManager();
     private String menuSelection;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +50,29 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupLogo();
+
+        // check if network is available on user's phone and if it is run Async task
+        if (isNetworkAvailable()) {
+            GetQuotesTask getQuotesTask = new GetQuotesTask();
+            getQuotesTask.execute();
+        }
+        else {
+            Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        boolean isAvailable = false;
+
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+
+        return isAvailable;
     }
 
     private void setupLogo(){
@@ -130,11 +170,34 @@ public class MainActivity extends Activity {
        this.menuSelection = selection;
    }
 
-  public String getSelection(){
+    public String getSelection(){
      //Allows for the retrieval of the current menuSelection
       return this.menuSelection;
-  }
+    }
 
+    private class GetQuotesTask extends AsyncTask<Object, Void, String> {
 
+        @Override
+        protected String doInBackground(Object... params) {
+            int responseCode = -1;
+            JSONObject jsonResponse;
+            StringBuilder builder = new StringBuilder();
+            HttpClient client = new DefaultHttpClient();
+            HttpGet httpget = new HttpGet("http://halfmoonstudios.com.au/ponder/quotes.json");
+
+            try {
+                HttpResponse response = client.execute(httpget);
+                StatusLine statusLine = response.getStatusLine();
+                responseCode = statusLine.getStatusCode();
+                Log.v(TAG, "Code: " + responseCode);
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    Log.v(TAG, "Success!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "Code: " + responseCode;
+        }
+    }
 
 }
